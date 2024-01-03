c3e8669619a85bc670a53c227668a9598aa217bf
marcelo <marceloaugustonegredo@gmail.com>
2024-01-03 18:25:41 -0300

    pycharm

--- app/authbase/admin.py
+++ app/authbase/admin.py
@@ -4,27 +4,24 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser
 
 
-
-
 class CustomUserAdmin(BaseUserAdmin):
-   
     model = CustomUser
-    list_display = ("email", "is_staff", "is_active",)
+    list_display = ("email", "is_staff", "is_active", "usuario",)
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
-        (None, {"fields": ("email", "password","username")}),
+        ("Admin", {"fields": ("email", "password", "username", "usuario")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
             "fields": (
-                "email","username", "password1", "password2", "is_staff",
-                "is_active", "groups", "user_permissions"
+                "email", "username", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions", "usuario",
             )}
-        ),
+         ),
     )
-    search_fields = ("username", "first_name", "last_name", "email")
+    search_fields = ("username", "first_name", "last_name", "email", 'usuario',)
     ordering = ("email",)
 
 
--- app/authbase/models.py
+++ app/authbase/models.py
@@ -1,80 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser , PermissionsMixin,BaseUserManager
-from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-class CustomUserManager(BaseUserManager): 
-    
-    def _create_user(self, username, email, password, **extra_fields):
-        if not email:
-            raise ValueError(_('Users must have an email address'))
-        if not username:
-            raise ValueError(_('The username field is required'))
-        user = self.model(email = self.normalize_email(email), username = username,  **extra_fields)
-        user.set_password(password)
-        user.save(using = self._db)
-        return user
-    
-    def create_user(self, username, email = None, password = None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(username, email, password, **extra_fields)            
-    
-    
-    def create_superuser(self, username, email = None, password = None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+class CustomUser(AbstractUser):
+    USER_LEVEL = (
+        ("CR", 'Criador'),
+        ("ED", 'Editor'),
+        ("SP", 'Supervisor'),
+    )
 
-        return self._create_user(username, email, password, **extra_fields)
-    
-class CustomUser(AbstractUser, PermissionsMixin):
-   
-    username = models.CharField(
-        _("username"),
-        max_length=150,unique=True,help_text=_("Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only." ),
-       
-        error_messages={ "unique": _("A user with that username already exists."),},
-    )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), blank=False,unique = True)
-    is_staff = models.BooleanField(
-        _("staff status"),
-        default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
-    )
-    is_active = models.BooleanField(
-        _("active"),
-        default = True,
-        help_text  = _(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
-        ),
-    )
-    
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ['email']
-    
-    
-    objects = CustomUserManager()
-    
+    usuario = models.CharField(max_length=2, blank=True, choices=USER_LEVEL, default='CR', )
+
     def __str__(self):
         return self.username
-    
-    
-
-    
-        
-        
-        
-        
-       
-
-  
